564dee4cac436e8b2afd34576ee89c83c0430162
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-02 06:51:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-pkg upgrade -y  
-pkg install -y proot-distro  
+pkg upgrade -y 
+
+# Shortcut 
+cat > $PREFIX/bin/ubuntu << EOF
+proot-distro login ubuntu
+EOF
+
+pkg install -y proot-distro
 proot-distro install ubuntu
 proot-distro login ubuntu
