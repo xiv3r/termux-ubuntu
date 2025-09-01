787c90f612e7732300596857fd36dc0cfd2838b9
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-02 05:07:21 +0800

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+apt update && apt upgrade -y  
+apt install -y xfce4 xfce4-goodies  
+apt install -y tightvncserver
+cat > .vnc/xstartup << EOF
+#!/bin/sh
+
+unset SESSION_MANAGER  
+unset DBUS_SESSION_BUS_ADDRESS  
+xrdb $HOME/.Xresources  
+startxfce4 & 
+EOF
+chmod +x .vnc/xstartup
