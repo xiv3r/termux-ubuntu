4cd1b4e495026358257d69e163d4f94cc9850e20
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-02 05:05:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,3 @@ pkg upgrade -y
 pkg install -y proot-distro  
 proot-distro install ubuntu
 proot-distro login ubuntu
-apt update && apt upgrade -y  
-apt install -y xfce4 xfce4-goodies  
-apt install -y tightvncserver
-cat > $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/.vnc/xstartup  << EOF
-#!/bin/sh
-
-unset SESSION_MANAGER  
-unset DBUS_SESSION_BUS_ADDRESS  
-xrdb $HOME/.Xresources  
-startxfce4 & 
-EOF
-chmod +x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/.vnc/xstartup
