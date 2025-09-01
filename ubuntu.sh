6129cdebf50ebd2de7f6c5f15983b6fedb4f90ba
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-02 06:47:37 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-apt update && apt upgrade -y  
-apt install -y xfce4 xfce4-goodies tightvncserver xrdp dbus-x11
+apt update
+apt upgrade -y  
+apt install -y xfce4 xfce4-goodies tightvncserver xrdp dbus-x11 neofetch
+
+# VNC startup 
+cat >> .bashrc << EOF
+vncserver
+neofetch
+EOF
+
 cat > .vnc/xstartup << EOF
 #!/bin/sh
 
 unset SESSION_MANAGER  
 unset DBUS_SESSION_BUS_ADDRESS  
 xrdb $HOME/.Xresources  
-startxfce4 & 
+startxfce4 &
 EOF
 chmod +x .vnc/xstartup
