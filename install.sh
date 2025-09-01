38ac04324367fe89e94d32c44b4ef972160f658f
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-02 07:11:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 pkg upgrade -y 
 
-# Shortcut 
+# Shortcut login
 cat > $PREFIX/bin/ubuntu << EOF
+#!/bin/sh
+termux-wake-lock
 proot-distro login ubuntu
 EOF
 chmod +x $PREFIX/bin/ubuntu
+
+# Uninstaller 
+cat > $PREFIX/bin/uninstall-ubuntu
+#!/bin/sh
+proot-distro remove ubuntu
+EOF
 
 pkg install -y proot-distro
 proot-distro install ubuntu
