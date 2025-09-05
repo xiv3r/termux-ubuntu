f8fcab8ffb8f9849c46c1abfcf53ac8c87427707
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-06 07:42:13 +0800

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dependencies 
+apt install -y gnome-shell gnome-terminal gnome-tweaks gnome-shell-extensions gnome-shell-extension-ubuntu-dock nautilus nano gedit dbus-x11 tigervnc-standalone-server
+
+# Vnv startup
+mkdir -p .vnc
+cat > .vnc/xstartup << EOF
+#!/bin/bash
+
+export XDG_CURRENT_DESKTOP="GNOME"
+service dbus start 
+gnome-shell --x11
+for file in $(find /usr -type f -iname "login1"); do rm -rf $file 
+done
+EOF
+chmod +x .vnc/xstartup
+
+# Startup
+cat >> .bashrc << EOF
+neofetch
+EOF
