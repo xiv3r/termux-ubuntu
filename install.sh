7a5f7247330f5f2ccfaeb65a302def0d93d55dd2
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-06 08:51:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pkg upgrade -y 
-
 # Shortcut login
+sed -i '/termux-wake-lock/d' $PREFIX/etc/bash.bashrc
+sed -i '/clear/d' $PREFIX/etc/bash.bashrc
+sed -i '/proot-distro login ubuntu/d' $PREFIX/etc/bash.bashrc
 cat >> $PREFIX/etc/bash.bashrc << EOF
 termux-wake-lock
 clear
@@ -12,13 +13,14 @@ chmod +x $PREFIX/bin/ubuntu
 
 # Manual Login
 cat > $PREFIX/bin/ubuntu << EOF
-pd sh ubuntu
+proot-distro login ubuntu
 EOF
 chmod +x $PREFIX/bin/ubuntu
 
 # Uninstaller 
 cat > $PREFIX/bin/uninstall-ubuntu << EOF
 #!/bin/sh
+
 proot-distro remove ubuntu
 sed -i '/clear/d' $PREFIX/etc/bash.bashrc
 sed -i '/termux-wake-lock/d' $PREFIX/etc/bash.bashrc
@@ -27,6 +29,8 @@ rm -f $PREFIX/bin/ubuntu
 rm -f $PREFIX/bin/uninstall-ubuntu
 EOF
 
+pkg upgrade -y
 pkg install -y proot-distro
 proot-distro install ubuntu
+sleep 5
 proot-distro login ubuntu
